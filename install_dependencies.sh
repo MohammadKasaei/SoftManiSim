f5db1e517debbd45b4f2efd8126bb09f0e7c1aff
MohammadKasaei <s.mohammadreza.kasaei@gmail.com>
2024-09-26 15:34:51 +0100

    update version

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,16 +10,16 @@ pip install matplotlib==3.8.2
 pip install collection
 
 # Install pybullet
-pip install pybullet
+pip install pybullet==3.2.6
 
 # Install stable-baselines3
-pip install stable-baselines3
+pip install stable-baselines3==2.3.2
 
 # Install getkey
-pip install getkey
+pip install getkey==0.6.5
 
 # Install open cv2
-pip install opencv-python
+pip install opencv-python==4.9.0.80
 
 
 # Install pill
